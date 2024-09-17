b74b5fd0ebda0a314c51a0007d088f6fe853e309
tmartin1284 <profesor.tomasmr@ies-azarquiel.es>
2024-09-17 20:21:15 +0200

    primera subida

---
+++ JS-BASICO VACIO/00-consola/app.js
@@ -0,0 +1,28 @@
+
+// mensaje general
+console.log('Hola mundo')
+// mensage de información
+console.info("Esto es información")
+
+//debugger
+
+// mensaje de advertencia
+console.warn("Advertencia, cuidado")
+//mensaje de error
+console.error("Error FATAL")
+
+
+alert('Mensaje')
+if (confirm('¿Desea abandonar la página?')) //  --> devuelve un booleano
+{
+       console.log('Se va a abandonar la página')
+}else
+{
+       console.log('NO Se va a abandonar la página')
+} 
+
+
+let edad = prompt('Dime tu edad') //  --> devolve una cadena
+console.log(edad)
+
+      //debugger
