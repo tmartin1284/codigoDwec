39aa7915a32609f72048ac948917c7311796b5ef
tmartin1284 <profesor.tomasmr@ies-azarquiel.es>
2024-09-17 19:55:21 +0200

    primera subida

---
+++ Tema1/app.js
@@ -0,0 +1,2 @@
+console.log("hola mundo ____3");
+document.writeln("<h1>hola mundoooooo 2</H1>");
---
+++ Tema2/script1.js
@@ -0,0 +1,34 @@
+
+
+
+
+/*
+alert ("esto es un ejemplo");
+console.log("esto es un ejemplo en la consola");
+
+
+prompt ("esto es un ejemplo");
+console.warn("esto es un ejemplo en la consola");
+
+
+
+confirm ("esto es un ejemplo de confirm");
+console.error("esto es un ejemplo confirm en la consola");
+*/
+
+var a = 57;
+var resultado;
+
+function manolo(  b) {
+
+     resultado = a + b;
+
+   
+
+}
+
+
+manolo (100);
+
+alert (resultado);
+
