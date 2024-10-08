db39fdba75af349867c1a18be3347f69d17c9871
tmartin1284 <profesor.tomasmr@ies-azarquiel.es>
2024-10-08 13:51:23 +0200

    Carlos, mira para que te entretengas

--- Tema4 DOM/Ejemplos/codigo.js
+++ Tema4 DOM/Ejemplos/codigo.js
@@ -2,29 +2,30 @@
 
 
 document.title="pagi77na web de prueba";
-console.log(document.getElementsByTagName("title")[0].text);
-let titulo= document.getElementsByTagName("title")[0];
-titulo.text="manolo, coñoo";
-console.log(titulo.nodeType);
+// console.log(document.getElementsByTagName("title")[0].text);
+// let titulo= document.getElementsByTagName("title")[0];
+// titulo.text="manolo, coñoo";
+// console.log(titulo.nodeType);
 
-for (let propiedad in titulo){ 
-     console.log(propiedad + " tiene el valor de " + titulo[propiedad])
+// for (let propiedad in titulo){ 
+//      console.log(propiedad + " tiene el valor de " + titulo[propiedad])
+// }
+
+// titulo.setAttribute("title","hola a todos")
+// titulo.setAttribute("id","485")
+// titulo.setAttribute("title","hola a todos")
+// console.log (titulo.outerHTML) 
+
+let cards=document.getElementsByClassName("card");
+console.log (cards.length);
+let i=0;
+
+const personajes =['billsjpeg', 'bobo', 'brolly', 'bubu', 'buhan', 'bulma', 'c18', 'cell', 'chau', 'cichi', 'freezer', 'gohan', 'goku', 'gotten', 'hero', 'karin', 'krilin', 'mutenroi', 'pam', 'piccolo', 'porunga', 'puer', 'shengon', 'tenshinhan', 'trunks', 'vegeta', 'videl', 'yamcha']
+
+for (let card of cards){
+     let img = card.getElementsByTagName("img")[0];
+img.src="./img/"+personajes[i]+".jpg"
+
+
 }
 
-titulo.setAttribute("title","hola a todos")
-titulo.setAttribute("id","485")
-titulo.setAttribute("title","hola a todos")
-console.log (titulo.outerHTML)
-
-/*
-
-const cards= document.getElementsByClassName("card");
-
-let contador =0;
-
-for (cars in cards){
-const titulo=card.getElementsByClassName("card-tittle")
-titulo.getElementsByClassName
-}
-
-*/
