bac824ef84bc832ed8cc22db0ce2dc0741d5afad
tmartin1284 <profesor.tomasmr@ies-azarquiel.es>
2024-10-08 21:41:19 +0200

    Caarlos

--- Tema4 DOM/Ejemplos/codigo.js
+++ Tema4 DOM/Ejemplos/codigo.js
@@ -47,7 +47,7 @@ for (let card of cards){
      /* obtenemos la imagen del card, y cambiamos el atributo source
      */
      let img = card.getElementsByTagName("img")[0];
-     img.src="./img/"+personajes[i]+".jpg";
+     img.src="./img/"+personajes[i]+".jpg"; //ojo, lo podemos hacer asi, o con el set attribute. mas abajo uso el otro
      img.setAttribute("border", i);
      img.setAttribute("id", i);
      /* cambiamos el titulo y la descripción del elemento.
@@ -88,20 +88,51 @@ for (let card of cards){
 
 const rowCard= document.getElementsByClassName("row")[0]; //de 0, poqeue nos devuelve un array
 let colCardModelo=rowCard.childNodes[0];
-console.log(colCardModelo.nodeName);
-i =0;
- //do colCardModelo=rowCard.childNodes[i++]; while (colCardModelo.nodeName!="DIV") //cojo el primer div que me encuentre
+i =1;
+while (colCardModelo.nodeName!="DIV")colCardModelo=rowCard.childNodes[i++]; //cojo el primer div que me encuentre
+ 
 
- console.log(colCardModelo);
+while (rowCard.firstChild) {
+     rowCard.removeChild(rowCard.lastChild);
+   }
+console.log(colCardModelo)
+
+/**
+ * ahora creamos una copia del colCardModelo, con el metodo que hay, le cambiamos los valores, y lo appendemos a los hijos del rowcard, todo dentro de un for
+ * 
+ * quien controla al for, pues los personajes de dragon vall que ya tenemos
+ * 
+ * copia:
+ *                  <div class="card">
+                        <img class="card-img-top" src="./img/hero.jpg" alt="Title" border="14" id="14">
+                        <div class="card-body">
+                            <h4 class="card-title">Hero</h4>
+                            <p class="card-text">Descripción del personaje Hero</p>
+                        <p class="card-text">segunda linea de la descripción de hero</p></div>
+                    </div>
+ * 
+ * 
+ */
 
 
-// for (let col of rowCard.childNodes){  //fijaos aqui, que además de recuperar los 8 nodos div, me recupera los 8 espacios en blanco, como nodos de texto vacios
+for (let personaje of personajes){
+     let copia=colCardModelo.cloneNode(true); //importante, crear una copia cada vez
+     let imagen=copia.getElementsByTagName("img")[0]; //primero la imagen
+     imagen.setAttribute("src","./img/"+personaje+".jpg");
+     imagen.setAttribute("alt","Imagen de "+personaje);
+     imagen.setAttribute("border",0);
+     imagen.setAttribute("id",personaje);
 
-//      console.log(col.nodeName+ col.nodeType)
-//      console.log(col.outerHTML)
-// }
-//      //rowCard.removeChild(col); // ahora elimino a todos los nodos. cuidado que me los cargo, salvo al primero, que esta guardado
+// ahora vamos con los pies de foto, tal y como está en el otro bucle
+     let nombre=copia.getElementsByTagName("h4")[0];
+     nombre.childNodes[0].nodeValue=UpperFirst(personaje);
+    let descr=copia.getElementsByTagName("p")[0];
+     descr.firstChild.nodeValue="Descripción del personaje "+UpperFirst(personaje);
+     descr=copia.getElementsByTagName("p")[1];
+    descr.childNodes[0].nodeValue="más descripción de "+personaje;
+     
+//y lo más importante, el objeto copia, lo metemos en el row card
+rowCard.appendChild(copia);
 
-
-
+}
 
