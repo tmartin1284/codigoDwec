fd10e5e398cbfb0bc97c0ff9cd712ec76697e148
tmartin1284 <profesor.tomasmr@ies-azarquiel.es>
2025-01-15 11:12:42 +0100

    fdsfa

--- Tema9/PrimerReact/src/App.jsx
+++ Tema9/PrimerReact/src/App.jsx
@@ -8,6 +8,34 @@ function App() {
 
   return (
     <>
+    
+  <div className="app">
+    <h1>My wishlist</h1>
+    <fieldset className="wish-input">
+      <legend className="wish-input__label">New wish</legend>
+      <input className="wish-input__field" placeholder="Enter your wish here" />
+    </fieldset>
+    <ul className="wish-list">
+      {wishes.map(({ done, text }, i) => (
+        <li
+          key={text}
+          className={classNames('wish-list__item', {
+            'wish-list__item--done': done,
+          })}
+        >
+          <input id={`wish${i}`} type="checkbox" checked={done} />
+          <label htmlFor={`wish${i}`}>{text}</label>
+        </li>
+      ))}
+    </ul>
+    <button type="button" className="wish-clear">
+      Archive done
+    </button>
+  </div>
+
+
+
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +44,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React tomas edition  version prueba</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
